30692c725bf903168b3109dcce4aace6abfd134d
Abhinav1004 <abhinavkrjha10@gmail.com>
2021-10-27 20:14:37 +0530

    added templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,29 +1,16 @@
-from django.shortcuts import render
-from django.utils import timezone
-from django.contrib.auth.models import User
+from django.shortcuts import render,get_object_or_404
+from .models import Post
 
-class Post(models.Model):
-    STATUS_CHOICES = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
-    )
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250,
-    unique_for_date='publish')
-    author = models.ForeignKey(User,
-    on_delete=models.CASCADE,
-    related_name='blog_posts')
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,
-    choices=STATUS_CHOICES,
-    default='draft')
-    
-    class Meta:
-        ordering = ('-publish',)
-    
-    def __str__(self):
-        return self.title
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,'blog/post/list.html',{'posts':posts})
+
+def post_detail(request,year,month,day,post):
+    posts =  get_object_or_404(Post,slug = post,
+                                    status='published',
+                                    publish__year=year,
+                                    publish__month=month,
+                                    publish__day=day)
+    # print(posts.body)
+    return render(request,'blog/post/detail.html',{'posts':posts})
+                                 
