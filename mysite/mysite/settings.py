fd960431ad790e2e1c06a6f6c0b024ea50bb047f
Abhinav1004 <abhinavkrjha10@gmail.com>
2021-11-05 17:59:21 +0530

    added search feature

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -81,8 +82,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': str(BASE_DIR / 'blogdb'),
+        'NAME':'blogdb',
+        'USER':'abhinav',
     }
 }
 
